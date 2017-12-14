878afc54da92ae00a27583ce7118a324f7b223b2
Trade System <romanenko.a@bk.ru>
2017-12-14 11:21:52 +0300

    Update InvalidBundleException.php

--- Exception/InvalidBundleException.php
+++ Exception/InvalidBundleException.php
@@ -9,7 +9,7 @@
  * with this source code in the file LICENSE.
  */
 
-namespace Symfony\Bundle\AsseticBundle\Exception;
+namespace TS\Bundle\AsseticBundle\Exception;
 
 class InvalidBundleException extends \LogicException
 {
