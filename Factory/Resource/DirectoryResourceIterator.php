b864d60f55e64d8280a9aa9b72b858765b0b18fa
Trade System <romanenko.a@bk.ru>
2017-12-14 11:23:12 +0300

    Update DirectoryResourceIterator.php

--- Factory/Resource/DirectoryResourceIterator.php
+++ Factory/Resource/DirectoryResourceIterator.php
@@ -9,7 +9,7 @@
  * with this source code in the file LICENSE.
  */
 
-namespace Symfony\Bundle\AsseticBundle\Factory\Resource;
+namespace TS\Bundle\AsseticBundle\Factory\Resource;
 
 use Symfony\Component\Templating\Loader\LoaderInterface;
 
