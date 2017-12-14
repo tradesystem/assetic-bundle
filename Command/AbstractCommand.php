76bf65dd46fec634c63c9f46bdc16e270f142429
Trade System <romanenko.a@bk.ru>
2017-12-14 11:16:01 +0300

    Update AbstractCommand.php

--- Command/AbstractCommand.php
+++ Command/AbstractCommand.php
@@ -9,7 +9,7 @@
  * with this source code in the file LICENSE.
  */
 
-namespace Symfony\Bundle\AsseticBundle\Command;
+namespace TS\Bundle\AsseticBundle\Command;
 
 use Assetic\Asset\AssetCollectionInterface;
 use Assetic\Asset\AssetInterface;
